d49ad9fbc00b74cd417c92ddb928125520ccff81
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-31 17:50:29 +0200

    Displaying genres.

--- src/components/GenresComponents/GenreList.tsx
+++ src/components/GenresComponents/GenreList.tsx
@@ -1,13 +1,20 @@
 import useGenres from "../../hooks/useGenres.ts";
+import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import getCroppedImageUrl from "../../services/image-url.ts";
 
 const GenreList = () => {
     const { data : genres} = useGenres();
     return (
-        <ul>
+        <List>
             {genres.map(genre => (
-                <li key={genre.id}>{genre.name}</li>
+                <ListItem key={genre.id} paddingY={1}>
+                    <HStack>
+                        <Image src={getCroppedImageUrl(genre.image_background)} boxSize='32px' borderRadius={5} />
+                        <Text fontSize='lg'>{genre.name}</Text>
+                    </HStack>
+                </ListItem>
             ))}
-        </ul>
+        </List>
     )
 }
 

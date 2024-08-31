0797c7c26d1be76d4e7cf5fbf0feba06769c7c36
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-31 15:21:12 +0200

    Building the game hook for fetching the games.

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+import apiClient from "../services/api-client.ts";
+import {CanceledError} from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGameResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+    const controller = new AbortController();
+
+    useEffect(() => {
+        apiClient.get<FetchGameResponse>('/games', { signal: controller.signal})
+            .then(res => setGames(res.data.results))
+            .catch(err => {
+                if(err instanceof CanceledError) return;
+                setError(err.message)
+            });
+        return () => controller.abort() ;
+    }, []);
+    return { games, error}
+}
+
+export default useGames;

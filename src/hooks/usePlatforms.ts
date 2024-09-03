585ccc436ae80898fe2695b3bd76b9820aa40c00
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-09-03 10:22:42 +0200

    Fetching the platforms using react query.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,17 @@
 import platforms from '../data/platforms.ts'
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+import {useQuery} from "@tanstack/react-query";
+import apiClient from "../services/api-client.ts";
+import {FetchResponse} from "./useData.ts";
+import {Platform} from "./useGames.ts";
+
+
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () =>
+        apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents')
+            .then(res => res.data.results),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: platforms
+});
 
 export default usePlatforms

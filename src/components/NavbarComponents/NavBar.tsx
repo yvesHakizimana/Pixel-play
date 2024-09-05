330a9e02a36ce417291cebd80c0bbb11a404905e
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-09-05 13:04:11 +0200

    Refactoring - : Fixing navbar and dashboard issues.

--- src/components/NavbarComponents/NavBar.tsx
+++ src/components/NavbarComponents/NavBar.tsx
@@ -2,14 +2,16 @@ import {HStack, Image} from "@chakra-ui/react";
 import logo from "../../assets/logo.webp"
 import ColorModeSwitch from "../ColorModeSwitch.tsx";
 import SearchInput from "./SearchInput.tsx";
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 
 const NavBar = () => {
     return (
         <>
             <HStack  padding='10px'>
-                <Image  src={logo} boxSize='60px'/>
+                <Link to='/'>
+                    <Image  src={logo} boxSize='60px' objectFit='cover'/>
+                </Link>
                 <SearchInput  />
                 <ColorModeSwitch />
             </HStack>
--- src/components/NavbarComponents/SearchInput.tsx
+++ src/components/NavbarComponents/SearchInput.tsx
@@ -2,15 +2,19 @@ import {Input, InputGroup, InputLeftElement} from "@chakra-ui/react";
 import {BsSearch} from "react-icons/bs";
 import {useRef} from "react";
 import useGameQueryStore from "../../store.ts";
+import {useNavigate} from "react-router-dom";
 
 const SearchInput = () => {
     const {setSearchText} = useGameQueryStore();
     const searchRef = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate();
     return (
         <form onSubmit={(event) => {
             event.preventDefault();
-            if(searchRef.current)
+            if(searchRef.current) {
                 setSearchText(searchRef.current.value);
+                navigate('/')
+            }
         }}>
             <InputGroup>
                 <InputLeftElement children={<BsSearch />} />
